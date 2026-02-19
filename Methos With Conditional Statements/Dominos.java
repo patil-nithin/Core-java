4c9f0aa1ad020d897b07bd5019fbd4bd03ab60fa
patil-nithin <nithinpatil237@gmail.com>
2026-02-19 23:16:43 +0530

    Conditional_statements

---
+++ Methos With Conditional Statements/Dominos.java
@@ -0,0 +1,176 @@
+class Dominos {
+
+    public static double getFoodPrice(String FoodName) {
+
+        double price = 0.0;
+
+        if (FoodName == "Margherita Pizza") {
+            price = 199;
+
+        } else if (FoodName == "Peppy Paneer Pizza") {
+            price = 249;
+
+        } else if (FoodName == "Veg Extravaganza") {
+            price = 299;
+
+        } else if (FoodName == "Chicken Dominator") {
+            price = 349;
+
+        } else if (FoodName == "Pepperoni Pizza") {
+            price = 329;
+
+        } else if (FoodName == "Cheese Burst Pizza") {
+            price = 319;
+
+        } else if (FoodName == "Mexican Green Wave") {
+            price = 239;
+
+        } else if (FoodName == "Indi Tandoori Paneer") {
+            price = 279;
+
+        } else if (FoodName == "Chicken Sausage Pizza") {
+            price = 269;
+
+        } else if (FoodName == "Deluxe Veggie Pizza") {
+            price = 259;
+
+        } else if (FoodName == "Farmhouse Pizza") {
+            price = 289;
+
+        } else if (FoodName == "Paneer Makhani Pizza") {
+            price = 279;
+
+        } else if (FoodName == "Veggie Paradise") {
+            price = 219;
+
+        } else if (FoodName == "Chicken Golden Delight") {
+            price = 309;
+
+        } else if (FoodName == "Non Veg Supreme") {
+            price = 359;
+
+        } else if (FoodName == "Cheese Garlic Bread") {
+            price = 129;
+
+        } else if (FoodName == "Stuffed Garlic Bread") {
+            price = 149;
+
+        } else if (FoodName == "Garlic Breadsticks") {
+            price = 109;
+
+        } else if (FoodName == "Taco Mexicana Veg") {
+            price = 159;
+
+        } else if (FoodName == "Taco Mexicana NonVeg") {
+            price = 179;
+
+        } else if (FoodName == "Veg Parcel") {
+            price = 99;
+
+        } else if (FoodName == "Chicken Parcel") {
+            price = 119;
+
+        } else if (FoodName == "Choco Lava Cake") {
+            price = 99;
+
+        } else if (FoodName == "Red Velvet Lava") {
+            price = 119;
+
+        } else if (FoodName == "Butterscotch Mousse Cake") {
+            price = 109;
+
+        } else if (FoodName == "Chocolate Brownie") {
+            price = 89;
+
+        } else if (FoodName == "Coke 500ml") {
+            price = 60;
+
+        } else if (FoodName == "Pepsi 500ml") {
+            price = 60;
+
+        } else if (FoodName == "Sprite 500ml") {
+            price = 60;
+
+        } else if (FoodName == "Fanta 500ml") {
+            price = 60;
+
+        } else if (FoodName == "Mirinda 500ml") {
+            price = 60;
+
+        } else if (FoodName == "Veg Loaded Pizza") {
+            price = 189;
+
+        } else if (FoodName == "Chicken Loaded Pizza") {
+            price = 219;
+
+        } else if (FoodName == "Veg Taco") {
+            price = 99;
+
+        } else if (FoodName == "Chicken Taco") {
+            price = 119;
+
+        } else if (FoodName == "Paneer Zingy Parcel") {
+            price = 109;
+
+        } else if (FoodName == "Chicken Zingy Parcel") {
+            price = 129;
+
+        } else if (FoodName == "Classic Corn Pizza") {
+            price = 199;
+
+        } else if (FoodName == "Double Cheese Pizza") {
+            price = 249;
+
+        } else if (FoodName == "BBQ Chicken Pizza") {
+            price = 329;
+
+        } else if (FoodName == "Cheesy Dip") {
+            price = 30;
+
+        } else if (FoodName == "Tandoori Dip") {
+            price = 30;
+
+        } else if (FoodName == "Peri Peri Dip") {
+            price = 30;
+
+        } else if (FoodName == "Veg Supreme Pizza") {
+            price = 299;
+
+        } else if (FoodName == "Chicken Supreme Pizza") {
+            price = 349;
+
+        } else if (FoodName == "Paneer & Onion Pizza") {
+            price = 229;
+
+        } else if (FoodName == "Capsicum Delight Pizza") {
+            price = 209;
+
+        } else if (FoodName == "Spicy Chicken Pizza") {
+            price = 319;
+
+        } else if (FoodName == "Cheese n Corn Pizza") {
+            price = 219;
+
+        } else if (FoodName == "Veggie Burger Pizza") {
+            price = 239;
+
+        } else if (FoodName == "Chicken Burger Pizza") {
+            price = 279;
+
+        } else if (FoodName == "Italiano Pizza") {
+            price = 299;
+
+        } else if (FoodName == "Spicy Veg Pizza") {
+            price = 219;
+
+        } else if (FoodName == "Classic Cheese Pizza") {
+            price = 199;
+
+        } else {
+            System.out.println("The Food Name is Not Found.");
+        }
+
+        return price;
+    }
+}
+ 
---
+++ Methos With Conditional Statements/SwiggyExecutor.java
@@ -0,0 +1,227 @@
+class SwiggyExecutor {
+
+    public static void main(String[] args) {
+
+        String FoodName;
+
+        FoodName = "Masala Dosa";
+        double ref1 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref1);
+
+        FoodName = "Rava Dosa";
+        double ref2 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref2);
+
+        FoodName = "Plain Dosa";
+        double ref3 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref3);
+
+        FoodName = "Idli Vada Combo";
+        double ref4 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref4);
+
+        FoodName = "Poori Sagu";
+        double ref5 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref5);
+
+        FoodName = "Bisibele Bath";
+        double ref6 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref6);
+
+        FoodName = "Puliyogare";
+        double ref7 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref7);
+
+        FoodName = "Curd Rice";
+        double ref8 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref8);
+
+        FoodName = "Tomato Rice";
+        double ref9 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref9);
+
+        FoodName = "Veg Biryani";
+        double ref10 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref10);
+
+        FoodName = "Chicken Biryani";
+        double ref11 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref11);
+
+        FoodName = "Mutton Biryani";
+        double ref12 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref12);
+
+        FoodName = "Paneer Butter Masala";
+        double ref13 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref13);
+
+        FoodName = "Butter Chicken";
+        double ref14 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref14);
+
+        FoodName = "Palak Paneer";
+        double ref15 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref15);
+
+        FoodName = "Chole Bhature";
+        double ref16 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref16);
+
+        FoodName = "Rajma Chawal";
+        double ref17 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref17);
+
+        FoodName = "Dal Tadka";
+        double ref18 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref18);
+
+        FoodName = "Jeera Rice";
+        double ref19 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref19);
+
+        FoodName = "Tandoori Roti";
+        double ref20 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref20);
+
+        FoodName = "Butter Naan";
+        double ref21 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref21);
+
+        FoodName = "Garlic Naan";
+        double ref22 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref22);
+
+        FoodName = "Veg Fried Rice";
+        double ref23 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref23);
+
+        FoodName = "Chicken Fried Rice";
+        double ref24 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref24);
+
+        FoodName = "Veg Manchurian";
+        double ref25 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref25);
+
+        FoodName = "Gobi Manchurian";
+        double ref26 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref26);
+
+        FoodName = "Chicken Manchurian";
+        double ref27 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref27);
+
+        FoodName = "Hakka Noodles";
+        double ref28 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref28);
+
+        FoodName = "Schezwan Noodles";
+        double ref29 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref29);
+
+        FoodName = "Egg Noodles";
+        double ref30 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref30);
+
+        FoodName = "Veg Spring Roll";
+        double ref31 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref31);
+
+        FoodName = "Chicken Spring Roll";
+        double ref32 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref32);
+
+        FoodName = "Samosa";
+        double ref33 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref33);
+
+        FoodName = "Kachori";
+        double ref34 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref34);
+
+        FoodName = "Pani Puri";
+        double ref35 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref35);
+
+        FoodName = "Bhel Puri";
+        double ref36 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref36);
+
+        FoodName = "Sev Puri";
+        double ref37 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref37);
+
+        FoodName = "Vada Pav";
+        double ref38 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref38);
+
+        FoodName = "Pav Bhaji";
+        double ref39 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref39);
+
+        FoodName = "Misal Pav";
+        double ref40 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref40);
+
+        FoodName = "Upma";
+        double ref41 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref41);
+
+        FoodName = "Kesari Bath";
+        double ref42 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref42);
+
+        FoodName = "Gulab Jamun";
+        double ref43 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref43);
+
+        FoodName = "Rasgulla";
+        double ref44 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref44);
+
+        FoodName = "Jalebi";
+        double ref45 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref45);
+
+        FoodName = "Kaju Katli";
+        double ref46 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref46);
+
+        FoodName = "Ice Cream Sundae";
+        double ref47 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref47);
+
+        FoodName = "Falooda";
+        double ref48 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref48);
+
+        FoodName = "Lassi";
+        double ref49 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref49);
+
+        FoodName = "Buttermilk";
+        double ref50 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref50);
+
+        FoodName = "Filter Coffee";
+        double ref51 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref51);
+
+        FoodName = "Masala Tea";
+        double ref52 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref52);
+
+        FoodName = "Cold Coffee";
+        double ref53 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref53);
+
+        FoodName = "Fresh Orange Juice";
+        double ref54 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref54);
+
+        FoodName = "Mango Milkshake";
+        double ref55 = Swiggy.getFoodPrice(FoodName);
+        System.out.println("The Price of the Food " + FoodName + " is : " + ref55);
+    }
+}
