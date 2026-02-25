11969ab26b98b40c94d7eba5078fcfd61303b641
patil-nithin <nithinpatil237@gmail.com>
2026-02-25 22:40:15 +0530

    Deleted some files and added some files

--- methodsWithParams/ZomatoExecutor.java
+++
@@ -1,9 +0,0 @@
-class ZomatoExecutor{
-
-public static void main(String [] args){
-
-    String foodName="Burger";
-	double ref= Zomato.search( foodName);
-	System.out.println("the price of " +foodName  + " is :"+ ref);
-	}
-	} 
