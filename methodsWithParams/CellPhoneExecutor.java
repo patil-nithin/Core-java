17a71a57e74c2de8e2965a259f27c228a297e352
patil-nithin <nithinpatil237@gmail.com>
2026-02-25 22:10:40 +0530

    Added array files

--- methodsWithParams/CellPhoneExecutor.java
+++
@@ -1,9 +0,0 @@
-class CellPhoneExecutor{
-
-public static void main(String [] args){
-
-    String contactName="Baba";
-	long anything= CellPhone.getContactNumber(contactName);
-	System.out.println("the contact No of " + contactName + " is :"+ anything);
-	}
-	} 
--- methodsWithParams/SpeakerExecutor.java
+++
@@ -1,8 +0,0 @@
-class SpeakerExecutor{
-	
-	public static void main (String[] speaker){
-		
-		//boolean ref = Speaker.onOrOff();
-		System.out.println("The speaker is connected : " + Speaker.onOrOff());
-	}
-}
